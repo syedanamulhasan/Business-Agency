8543008ebcbf12e0ff39722ed30a1a3db4bf38b4
syedanamulhasan <syedanamulhasan@gmail.com>
2024-10-19 20:35:01 +0600

    section 2 js done

--- script.js
+++ script.js
@@ -6,3 +6,34 @@ menu.addEventListener('click',()=>{
     menu.classList.toggle('change')
 })
 // end of navbar
+
+// section 2 video
+const video = document.querySelector('.video')
+const btn = document.querySelector('.buttons i')
+const bar = document.querySelector('.video-bar')
+
+const playPause = () =>{
+    if(video.paused){
+        video.play()
+    btn.className='far fa-pause-circle'
+    video.style.opacity='0.7'
+    }else{
+        video.pause()
+    btn.className='far fa-play-circle'
+    video.style.opacity='0.3'
+    }
+    
+}
+btn.addEventListener('click',()=>{
+    playPause()
+})
+
+video.addEventListener('timeupdate',()=>{
+    const barWidth = video.currentTime/video.duration
+    bar.style.width=`${barWidth*100}%`
+    if(video.ended){
+        btn.className ='far fa-play-circle'
+        video.style.opacity='0.3'
+    }
+})
+// end of section 2 video
